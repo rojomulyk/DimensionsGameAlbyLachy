a52c09b8e16faf35097c17a277be26739364e7b4
Lachlan <LightYearz@iinet.net.au>
2018-10-16 16:54:19 +1030

    added buttons

---
+++ Dimensions/Assets/Scripts/ButtonPad.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPad : MonoBehaviour {
+
+    public Material On;
+    public Material Off;
+
+    public AudioSource Source;
+    public AudioClip ClipOn;
+    public AudioClip ClipOff;
+
+    public Collider Base;
+
+    bool Pressed;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        if(Pressed == true)
+        {
+            gameObject.GetComponent<Renderer>().material = On;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material = Off;
+        }
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other != Base)
+        {
+            Pressed = true;
+            Source.clip = ClipOn;
+            Source.Play();
+        }
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != Base)
+        {
+            Pressed = false;
+            Source.clip = ClipOff;
+            Source.Play();
+        }
+    }
+
+}
