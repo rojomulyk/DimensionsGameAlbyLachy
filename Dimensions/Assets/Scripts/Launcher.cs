30f91a8425582e9b409fb076fdc78e875279cb02
XO-Doctor <44132391+XO-Doctor@users.noreply.github.com>
2018-10-17 19:06:21 +1030

    hi working on launchers
    nothing happening yet

---
+++ Dimensions/Assets/Scripts/Launcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+
+    public float LauncherForce;
+    public GameObject Players;
+    public Rigidbody Playerrb;
+
+    private void Start()
+    {
+        Playerrb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Playerrb.AddForce(0, 60, 10 * Time.deltaTime);
+    }
+
+}
