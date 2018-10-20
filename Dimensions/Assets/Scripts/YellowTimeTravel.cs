11430bf4fb0519b2107f453734718cb6e14b9bf5
XO-Doctor <44132391+XO-Doctor@users.noreply.github.com>
2018-10-20 22:11:40 +1030

    so close to yellow dimesion done :)

---
+++ Dimensions/Assets/Scripts/YellowTimeTravel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowTimeTravel : MonoBehaviour {
+
+    public class Prevpos
+    {
+        public Vector3 Loc;       
+
+        public Prevpos(Vector3 NewLoc)
+        {
+            Loc = NewLoc;
+        }
+    }
+
+}
---
+++ Dimensions/Assets/Scripts/YellowTimeTravelList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowTimeTravelList : MonoBehaviour {
+
+    public GameObject Player;
+    public Vector3 Vector;
+    public List<Vector3> vecs = new List<Vector3>();
+    public bool A = false;
+
+    public void Start()
+    {
+        A = true;       
+    }
+
+    private void FixedUpdate()
+    {
+        Vector = Player.transform.position;
+    }
+
+    void Update()
+    {
+        if (A == true)
+        {
+            StartCoroutine(Wait());
+        }
+        if (vecs.Count > (100F)) {
+            A = false;
+        }
+    }
+
+    public IEnumerator Wait()
+    {
+        A = false;
+        yield return new WaitForSeconds(0.1F);
+        vecs.Add(Vector);
+        A = true;       
+    }
+}
